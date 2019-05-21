b1c8d1e1c72b4f25fb2f91803acdbcec610339e6
Fernando <fergonzaramos@yahoo.es>
2019-05-21 03:34:01 +0200

    Last changes implementes

--- follow_person/src/pd_algorithm.cpp
+++ follow_person/src/pd_algorithm.cpp
@@ -3,18 +3,21 @@
 #include "follow_person/PersonFollowedData.h"
 #include "std_msgs/Int32.h"
 #include "std_msgs/Float32.h"
+#include "std_msgs/String.h"
 #include "sensor_msgs/CameraInfo.h"
+#include <bica/Component.h>
 #include <cmath>
 
 const float Pi = 3.1416;
 
-class pdAlgorithm
+class pdAlgorithm: public bica::Component
 {
 
 private:
   ros::NodeHandle n_;
   ros::Subscriber sub_, info_sub_;
-  ros::Publisher speed_pub_;
+  ros::Publisher speed_pub_, talk_publisher_;
+  std_msgs::String data_;
 
   int width;
   float minDist, maxDist, Kp, Kd, prevErrorLinear, prevErrorAngular, maxV, maxW, prevV;
@@ -25,13 +28,15 @@ public:
     sub_ = n_.subscribe("/person_followed_data", 1, &pdAlgorithm::cb, this);
     info_sub_ = n_.subscribe("/camera/rgb/camera_info", 1, &pdAlgorithm::cb_info, this);
     speed_pub_ = n_.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1);
+    talk_publisher_ = n_.advertise<std_msgs::String>("/talk", 1);
+
 
     width = 0;
-    minDist = 700.0;
-    maxDist = 1000.0;
+    minDist = 900.0;
+    maxDist = 1200.0;
     maxV = 0.6;
-    Kp = 0.4;
-    Kd = 0.6;
+    Kp = 0.3;
+    Kd = 0.7;
     prevV = 0;
     maxW = Pi / 2;
     prevErrorLinear = 0;
@@ -42,6 +47,9 @@ public:
   {
     //Calculo la salida del control proporcional
     if(d > maxDist){
+      std_msgs::String s;
+      //s.data = "Come closer, please";
+      //talk_publisher_.publish(s);
       d = maxDist;
     }
     float currentErrorLinear = (d - minDist) / (maxDist - minDist); //error entre 0 y 1
@@ -50,8 +58,8 @@ public:
 
     prevErrorLinear = currentErrorLinear;
     float v = output * maxV;
-    if(abs(v - prevV) >= maxV / 4.0){
-      v = v / 4.0;
+    if(abs(v - prevV) >= maxV / 8.0){
+      v = v / 8.0;
     }
     prevV = v;
     return v;
@@ -71,7 +79,6 @@ public:
   void cb(const follow_person::PersonFollowedData::ConstPtr& msg)
   {
     if(width != 0){
-
       float dist = msg->dist;
       int centralPixel = msg->centralPixel;
       float v = pdAlgorithm::linear(dist);
--- number_persons_recognition/src/people_stimator.cpp
+++ number_persons_recognition/src/people_stimator.cpp
@@ -3,46 +3,68 @@
 #include "darknet_ros_msgs/BoundingBoxes.h"
 #include "darknet_ros_msgs/BoundingBox.h"
 #include "std_msgs/Int32.h"
+#include "std_msgs/String.h"
 #include "number_persons_recognition/BoundingBoxPersonArray.h"
 #include "number_persons_recognition/BoundingBoxPerson.h"
 #include <vector>
 #include <time.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string>
 
-const float MinPersonProb = 0.65;
-ros::Publisher person_stimation_publisher;
+const float MinPersonProb = 0.7;
 
-void cb(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msg)
+class Stimator
 {
-  int boxessize = msg->bounding_boxes.size();
-  std::vector<number_persons_recognition::BoundingBoxPerson> v;
-  number_persons_recognition::BoundingBoxPerson element;
-  for (int i = 0; i < boxessize; i++)
+private:
+  ros::NodeHandle n;
+  ros::Publisher person_stimation_publisher;
+  ros::Subscriber sub_node, object_subscriber;
+  std::string obj;
+public:
+  Stimator()
   {
-    if(msg->bounding_boxes[i].Class == "person"){
-      if(msg->bounding_boxes[i].probability > MinPersonProb){
+    obj = "person";
+    person_stimation_publisher = n.advertise<number_persons_recognition::BoundingBoxPersonArray>("/person_stimate", 1);
+    sub_node = n.subscribe("/darknet_ros/bounding_boxes", 1, &Stimator::cb, this);
+    object_subscriber = n.subscribe("/orders", 1, &Stimator::objectCallback, this);
+  }
 
-        element.xmin = msg->bounding_boxes[i].xmin;
-        element.xmax = msg->bounding_boxes[i].xmax;
-        element.ymin = msg->bounding_boxes[i].ymin;
-        element.ymax = msg->bounding_boxes[i].ymax;
-        v.push_back(element);
+
+  void cb(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msg)
+  {
+    int boxessize = msg->bounding_boxes.size();
+    std::vector<number_persons_recognition::BoundingBoxPerson> v;
+    number_persons_recognition::BoundingBoxPerson element;
+    for (int i = 0; i < boxessize; i++)
+    {
+      if(msg->bounding_boxes[i].Class == obj){
+        if(msg->bounding_boxes[i].probability > MinPersonProb){
+
+          element.xmin = msg->bounding_boxes[i].xmin;
+          element.xmax = msg->bounding_boxes[i].xmax;
+          element.ymin = msg->bounding_boxes[i].ymin;
+          element.ymax = msg->bounding_boxes[i].ymax;
+          v.push_back(element);
+        }
       }
     }
+    number_persons_recognition::BoundingBoxPersonArray a;
+    a.persons_array = v;
+    person_stimation_publisher.publish(a);
   }
-  number_persons_recognition::BoundingBoxPersonArray a;
-  a.persons_array = v;
-  person_stimation_publisher.publish(a);
-}
+
+  void objectCallback(const std_msgs::String::ConstPtr& msg)
+  {
+    obj = msg->data;
+  }
+
+};
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "darknet_ros_subscriber");
-  ros::NodeHandle n;
-  person_stimation_publisher = n.advertise<number_persons_recognition::BoundingBoxPersonArray>("/person_stimate", 1);
-  ros::Subscriber sub_node = n.subscribe("/darknet_ros/bounding_boxes",
-                                          1, cb);
+  Stimator stimator;
   ros::spin();
   /*
   while(ros::ok()){
